062253cc545449845cbafdbf6544670b220448fd
Eric Lundby <Eric.Lundby@gmail.com>
2026-07-01 08:50:33 -0600

    Dont copy lz4

--- recipe/install_db.bat
+++ recipe/install_db.bat
@@ -1,3 +1,1 @@
 meson install --no-rebuild -C build
-:: lz4-c conda package ships liblz4.dll but postgres links against lz4.dll
-if not exist %LIBRARY_BIN%\lz4.dll copy %LIBRARY_BIN%\liblz4.dll %LIBRARY_BIN%\lz4.dll
