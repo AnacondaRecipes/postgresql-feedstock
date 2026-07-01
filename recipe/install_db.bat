cc6acb758fdc6815a29b8335aa20f86fabd261c5
imykhailov <imykhailov@anaconda.com>
2026-07-01 12:48:22 +0300

    PKG-15996: copy liblz4.dll -> lz4.dll on Windows (name mismatch)

--- recipe/install_db.bat
+++ recipe/install_db.bat
@@ -1,1 +1,3 @@
 meson install --no-rebuild -C build
+:: lz4-c conda package ships liblz4.dll but postgres links against lz4.dll
+if not exist %LIBRARY_BIN%\lz4.dll copy %LIBRARY_BIN%\liblz4.dll %LIBRARY_BIN%\lz4.dll
